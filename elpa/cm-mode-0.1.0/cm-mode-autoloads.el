b86af7a1324c36be6d03c12fef6c2b345dca9c6e
Alex Baranosky <alexander.baranosky@gmail.com>
2014-10-01 22:38:04 -0400

    install rust-mode

---
+++ elpa/cm-mode-0.1.0/cm-mode-autoloads.el
@@ -0,0 +1,29 @@
+;;; cm-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (cm-mode) "cm-mode" "cm-mode.el" (21548 47712 0
+;;;;;;  0))
+;;; Generated autoloads from cm-mode.el
+
+(autoload 'cm-mode "cm-mode" "\
+
+
+\(fn MODE)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("cm-mode-pkg.el") (21548 47712 474072
+;;;;;;  0))
+
+;;;***
+
+(provide 'cm-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; cm-mode-autoloads.el ends here
